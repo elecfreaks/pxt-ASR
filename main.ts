b8666059f4200ca11595db32855741238e534093
Lionkk <Lion_yan@outlook.com>
2019-11-28 17:20:10 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,44 +1,25 @@
-enum vocabulary {
-    //% block="Open Light"
-    Type_x,
+enum vocabularyList {
+    //% block="Open Light" 
+    Open_Light,
     //% block="Close Light"
-    Type_y,
-    //% block="H"
-    Type_h,
-    //% block="W"
-    Type_w
+    Close_Light
 }
 
 //% weight=100 color=#0fbc11 icon="\uf130"
+//% block="ASR"
 namespace asr {
 
-    function i2cwrite(addr: number, reg: number, value: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = reg
-        buf[1] = value
-        pins.i2cWriteBuffer(addr, buf)
-    }
-
-    function i2ccmd(addr: number, value: number) {
-        let buf = pins.createBuffer(1)
-        buf[0] = value
-        pins.i2cWriteBuffer(addr, buf)
-    }
-
-    function i2cread(addr: number, reg: number) {
-        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
-        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
-        return val;
-    }
-
     //% block="the microphone hears %vocabulary"
-    export function asrmain(vocabulary: vocabulary): number {
-        let zbx
-        let buffer = pins.i2cReadBuffer(11, 1)
-        if (vocabulary == 0) {
-            zbx = buffer[0]
+    export function asrmain(vocabulary: vocabularyList): boolean {
+        let buffer = pins.i2cReadBuffer(0x11, 1)
+
+        if (buffer[0] == 0x01 && vocabulary == 1) {
+            return true
         }
-        return zbx
+        else{
+            return false
+        }
+        
     }
 
 
